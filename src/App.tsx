b1b2d31ad829eb70c2ea69bf40547abe880f3931
Roman Loboda <hyawey2003@gmail.com>
2025-07-12 18:47:30 -0400

    Main Page

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { NavBar } from './components/Nav/NavBar';
+
+
+function App() {
+
+  return (
+    <div>
+      <header className="ml-12 mr-12 mt-8">
+        <NavBar/>
+      </header>
+      <main className="">
+
+      </main>
+      <footer className="">
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
